3bc74079f4e85e06d232c7ab0d82a700be08fea4
jpabloallin <jpac.647@gmail.com>
2022-08-02 22:06:05 -0500

    first commit

---
+++ server/models/users.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true,
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    password: {
+        type: String,
+        require: true,
+        minlength: [8, "Password must be at least 8 characters long"],
+        select: false,
+    },
+
+    avatar: {
+        public_id: String,
+        url: String,
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+
+    tasks: [
+        {
+            title: String,
+            description: String,
+            completed: Boolean,
+            createdAt: Date,
+        },
+    ],
+
+    otp: Number,
+    otp_expiry: Date,
+});
+
+export const User = mongoose.model('User', userSchema);
